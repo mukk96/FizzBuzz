610ae6fbb6e122324b2575ed8ef07aa3f6d9d062
Kuroda Munetaka <mukk96@gmail.com>
2026-02-11 16:38:01 +0900

    Perl版のソースを追加

---
+++ fizzbuzz.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+# encoding: utf-8
+
+sub fizzbuzz
+{
+    my $n = shift @_;
+
+    if( $n % 15 == 0 ){
+        print "FizzBuzz\n" ;
+    }elsif( $n % 3 == 0 ){
+        print "Fizz\n";
+    }elsif( $n % 5 == 0 ){
+        print "Buzz\n";
+    }else{
+        print $n . "\n";
+    }
+}
+
+
+for($i = 0; $i <= 100; $i++){
+    &fizzbuzz($i);
+}
